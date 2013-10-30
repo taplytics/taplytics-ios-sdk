c28322dadbe0b7054427127bbc00280133e6630e
jonathannorris <jonathan@syrp.com>
2013-10-30 14:25:56 -0400

    added LPTableView

---
+++ LaunchPad.framework/Versions/A/Headers/LPTableView.h
@@ -0,0 +1,20 @@
+//
+//  LPTableView.h
+//  LaunchPad
+//
+//  Copyright (c) 2013 Syrp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LPElementProtocol.h"
+
+@interface LPTableView : UITableView <LPElementProtocol>
+
+- (id)initWithElementKey:(NSString*)elementKey styleKey:(NSString*)styleKey;
+
+- (id)initWithFrame:(CGRect)frame elementKey:(NSString*)elementKey styleKey:(NSString*)styleKey;
+
+- (id)initWithFrame:(CGRect)frame style:(UITableViewStyle)style
+         elementKey:(NSString*)elementKey styleKey:(NSString*)styleKey;
+
+@end
