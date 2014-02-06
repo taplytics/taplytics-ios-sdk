e438bb316fe04574fa36d12cd17e0b531b66fc98
jonathannorris <jonathan@syrp.com>
2014-02-05 17:19:44 -0800

    Added code based experiments

--- Taplytics.framework/Versions/A/Headers/Taplytics.h
+++ Taplytics.framework/Versions/A/Headers/Taplytics.h
@@ -22,6 +22,28 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
 + (void)startTaplyticsAPIKey:(NSString*)apiKey options:(NSDictionary*)options;
 
 /**
+ Run code experiment with experiment defined by experimentName, one baseline block or variation block will be run synchronously.
+ If no experiment has been defined the baseline block will be called. variation blocks are defined in a NSDictionary
+ with a key of the variation name, and a value of TLExperimentBlock. For Example:
+ 
+ [Taplytics runCodeExperiment:@"testExperiment" withBaseline:^(NSDictionary *variables) {
+ 
+ } variations:@{@"variation1": ^(NSDictionary *variables) {
+ 
+ }, @"variation2": ^(NSDictionary *variables) {
+ 
+ }}];
+ */
++ (void)runCodeExperiment:(NSString*)experimentName withBaseline:(TLExperimentBlock)baselineBlock variations:(NSDictionary*)variationNamesAndBlocks;
+
+/**
+ Report that an experiment goal has been achieved, optionally pass number value to track goal such as purchase revenue.
+ */
++ (void)goalAchieved:(NSString*)goalName;
+
++ (void)goalAchieved:(NSString*)goalName value:(NSNumber*)value;
+
+/**
  Updates Taplytics experiment properties in a background fetch, only avaliable in iOS 7
  @param completionBlock completion block called when fetch is complete
  */
