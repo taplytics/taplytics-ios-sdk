fd43b2924306be9e36c3287d76dc2861382adc76
jonathannorris <jonathan@syrp.com>
2014-03-12 16:30:03 -0700

    Taplytics 1.2.36 Release
    - built for iOS 7.1
    - updated event saving to disk

--- Taplytics.framework/Versions/A/Headers/Taplytics.h
+++ Taplytics.framework/Versions/A/Headers/Taplytics.h
@@ -5,7 +5,7 @@
 //  Copyright (c) 2014 Syrp Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "TaplyticsManager.h"
 
 typedef void(^TLExperimentBlock)(NSDictionary *variables);
@@ -29,11 +29,27 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
 /**
  Start the Taplytics SDK with your api key. the api key can be found in the 'project settings' page.
  @param apiKey your api key
- @param options options dictionary, used for testing, set @{"liveUpdate":@NO} to force app store mode, can be nil
  */
 + (void)startTaplyticsAPIKey:(NSString*)apiKey;
 
+/**
+ Start the Taplytics SDK with your api key. the api key can be found in the 'project settings' page.
+ @param apiKey your api key
+ @param options taplytics options dictionary, used for testing. Options inclue:
+            - @{"liveUpdate":@NO} to force production mode, or @YES to force live update mode for testing.
+            - @{"delayLoad":@2} allows Taplytics to show your launch image and load its configuration for a maximum number of seconds
+                on app startup. This is useful when running experiments on the first screen of your app, so users will get shown a variation 
+                on the first launch of your app.
+ */
+ 
 + (void)startTaplyticsAPIKey:(NSString*)apiKey options:(NSDictionary*)options;
+
+/**
+ Updates Taplytics configuration in a background fetch, only avaliable in iOS 7. It is HIGHLY recomended to implement background fetch
+ in your apps, to allow Taplytics to have an upto date configuration on app launch.
+ @param completionBlock completion block called when fetch is complete
+ */
++ (void)performBackgroundFetch:(void(^)(UIBackgroundFetchResult result))completionBlock NS_AVAILABLE_IOS(7_0);
 
 /**
  Optionally set the taplytics delegate when you need to know when a experiment has changed. For example if you are testing 
@@ -43,9 +59,12 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
 + (void)setTaplyticsDelegate:(id<TaplyticsDelegate>)delegate;
 
 /**
- Run code experiment with experiment defined by experimentName, one baseline block or variation block will be run synchronously.
- If no experiment has been defined the baseline block will be called. variation blocks are defined in a NSDictionary
- with a key of the variation name, and a value of TLExperimentBlock. For Example:
+ Run code experiment with experiment defined by experimentName, one baseline or variation block will be run synchronously.
+ If the "delayLoad" option is set in the options dictionary of startTaplyticsAPIKey:options: the block will be called asynchronously
+ once the Taplytics configuration has been loaded, but before the launch image is hidden.
+ 
+ If no experiment has been defined or no configuration has been loaded the baseline block will be called. 
+ Variation blocks are defined in a NSDictionary with a key of the variation name, and a value of TLExperimentBlock. For Example:
  
  [Taplytics runCodeExperiment:@"testExperiment" withBaseline:^(NSDictionary *variables) {
  
@@ -63,14 +82,6 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
 + (void)goalAchieved:(NSString*)goalName;
 
 + (void)goalAchieved:(NSString*)goalName value:(NSNumber*)value;
-
-/**
- Updates Taplytics experiment properties in a background fetch, only avaliable in iOS 7
- @param completionBlock completion block called when fetch is complete
- */
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
-+ (void)performBackgroundFetch:(void(^)(UIBackgroundFetchResult))completionBlock;
-#endif
 
 /**
  DEPRECATED Start Taplytics Methods, please use startTaplyticsAPIKey:options:
