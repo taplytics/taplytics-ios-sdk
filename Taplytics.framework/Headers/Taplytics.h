8e5ebdf8174ee71ed65e7fc38482823c9363a9c2
Jonathan Norris <jonathannorris@gmail.com>
2016-05-09 10:07:55 -0400

    Taplytics 2.10.26 Release
    - added TaplyticsOptionDisableBorders start option

--- Taplytics.framework/Headers/Taplytics.h
+++ Taplytics.framework/Headers/Taplytics.h
@@ -1,6 +1,6 @@
 //
 //  Taplytics.h
-//  Taplytics v2.10.24
+//  Taplytics v2.10.26
 //
 //  Copyright (c) 2015 Taplytics Inc. All rights reserved.
 //
--- Taplytics.framework/Headers/TaplyticsOptions.h
+++ Taplytics.framework/Headers/TaplyticsOptions.h
@@ -13,6 +13,7 @@ extern NSString *const TaplyticsOptionSessionBackgroundTime;
 extern NSString *const TaplyticsOptionPushSandbox;
 extern NSString *const TaplyticsOptionTestExperiments;
 extern NSString *const TaplyticsOptionDisable;
+extern NSString *const TaplyticsOptionDisableBorders;
 
 extern NSString *const TaplyticsDisablePushNotifications;
 extern NSString *const TaplyticsDisableLocationRegions;
